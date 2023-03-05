1fe686f77ce92c51b1c315af84399ea7ed3c84ab
Adams Banjo <sajioloye@gmail.com>
2023-03-04 21:34:47 -0500

    set up refresh token

--- api/server.go
+++ api/server.go
@@ -1,23 +1,32 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	db "github.com/sajitron/travel-agency/db/sqlc"
+	"github.com/sajitron/travel-agency/token"
 	"github.com/sajitron/travel-agency/util"
 )
 
 type Server struct {
-	config util.Config
-	router *gin.Engine
-	store  db.Store
+	config     util.Config
+	router     *gin.Engine
+	tokenMaker token.Maker
+	store      db.Store
 }
 
+// NewServer creates a new server and sets up routing
 func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewJWTMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("unable to initialise token maker: %w", err)
+	}
 	server := &Server{
-		config: config,
-		store:  store,
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
 	}
 
 	server.setupRouter()
--- db/sqlc/models.go
+++ db/sqlc/models.go
@@ -6,7 +6,20 @@ package db
 
 import (
 	"time"
+
+	"github.com/google/uuid"
 )
+
+type Sessions struct {
+	ID           uuid.UUID `json:"id"`
+	Email        string    `json:"email"`
+	RefreshToken string    `json:"refresh_token"`
+	UserAgent    string    `json:"user_agent"`
+	ClientIp     string    `json:"client_ip"`
+	IsBlocked    bool      `json:"is_blocked"`
+	ExpiresAt    time.Time `json:"expires_at"`
+	CreatedAt    time.Time `json:"created_at"`
+}
 
 type Users struct {
 	ID                int64     `json:"id"`
